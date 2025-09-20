b545c5af69428777faece0be7fc5f6d8835a6a70
Kiet dang <dang@ad-hoc.com>
2025-09-20 15:05:11 +0200

    temp2

--- elpa-elpa4/init-service.sh
+++ elpa-elpa4/init-service.sh
@@ -35,36 +35,88 @@ cp -r blueprint "$BLUEPRINT_DIR"
 TARGET_DIR="envs/$env/$service_name"
 mkdir -p "$TARGET_DIR"
 
-find "$BLUEPRINT_DIR" -type f -name "*.yaml" -exec sed -i "s|<env>|$env|g" {} +
-find "$BLUEPRINT_DIR" -type f -name "*.yaml" -exec sed -i "s|<service-name>|$service_name|g" {} +
-find "$BLUEPRINT_DIR" -type f -name "*.yaml" -exec sed -i "s|<image-name>|$image_name|g" {} +
-find "$BLUEPRINT_DIR" -type f -name "*.yaml" -exec sed -i "s|<image-tag>|$image_tag|g" {} +
+# Fixed sed commands - process each file individually instead of using find with +
+find "$BLUEPRINT_DIR" -type f -name "*.yaml" -exec sed -i.bak "s|<env>|$env|g" {} \;
+find "$BLUEPRINT_DIR" -type f -name "*.yaml" -exec sed -i.bak "s|<service-name>|$service_name|g" {} \;
+find "$BLUEPRINT_DIR" -type f -name "*.yaml" -exec sed -i.bak "s|<image-name>|$image_name|g" {} \;
+find "$BLUEPRINT_DIR" -type f -name "*.yaml" -exec sed -i.bak "s|<image-tag>|$image_tag|g" {} \;
+
+# Clean up backup files created by sed
+find "$BLUEPRINT_DIR" -name "*.bak" -delete
 
 # Anpassen der Kustomization-Datei für Komponenten und Literale
 COMPONENTS=""
 LITERALS=""
 
 if [[ "$use_postgres" =~ ^(ja|j)$ ]]; then
-  COMPONENTS+="\n  - ../../base/components/postgres\n  - ../components/postgres"
-  LITERALS+="\n    - POSTGRES_SCHEMA=$postgres_schema_name"
+  COMPONENTS+="  - ../../base/components/postgres\n  - ../components/postgres"
+  if [[ -n "$postgres_schema_name" ]]; then
+    LITERALS+="      - POSTGRES_SCHEMA_NAME=$postgres_schema_name"
+  fi
 fi
 
 if [[ "$use_kafka" =~ ^(ja|j)$ ]]; then
-  COMPONENTS+="\n  - ../../base/components/kafka\n  - ../components/kafka"
+  if [[ -n "$COMPONENTS" ]]; then
+    COMPONENTS+="\n"
+  fi
+  COMPONENTS+="  - ../../base/components/kafka\n  - ../components/kafka"
 fi
 
-# Hinweis: MongoDB hat in den bereitgestellten Beispielen keine `kustomization.yaml` im `dev`-Verzeichnis, daher wird nur der Basispfad hinzugefügt.
 if [[ "$use_mongodb" =~ ^(ja|j)$ ]]; then
-  COMPONENTS+="\n  - ../../base/components/mongodb"
+  if [[ -n "$COMPONENTS" ]]; then
+    COMPONENTS+="\n"
+  fi
+  COMPONENTS+="  - ../../base/components/mongo\n  - ../components/mongo"
 fi
 
 if [[ "$use_auth_url" =~ ^(ja|j)$ ]]; then
-  LITERALS+="\n    - AUTH_URL=$auth_url"
+  if [[ -n "$LITERALS" ]]; then
+    LITERALS+="\n"
+  fi
+  LITERALS+="      - AUTH_URL=$auth_url"
 fi
 
-# Ersetzen der Platzhalter in den YAML-Dateien
-find "$BLUEPRINT_DIR" -type f -name "kustomization.yaml" -exec sed -i "/<component-list>/c$COMPONENTS" {} +
-find "$BLUEPRINT_DIR" -type f -name "kustomization.yaml" -exec sed -i "/<literal-list>/c$LITERALS" {} +
+# Handle the case where no postgres schema is provided but postgres is used
+if [[ "$use_postgres" =~ ^(ja|j)$ ]] && [[ -z "$postgres_schema_name" ]]; then
+  if [[ -n "$LITERALS" ]]; then
+    LITERALS+="\n"
+  fi
+  LITERALS+="      - POSTGRES_SCHEMA_NAME=<postgres-schema-name>"
+fi
+
+# Replace placeholders in kustomization.yaml
+if [[ -f "$BLUEPRINT_DIR/kustomization.yaml" ]]; then
+  if [[ -n "$COMPONENTS" ]]; then
+    # Use printf to handle newlines properly and escape for sed
+    COMPONENTS_ESCAPED=$(printf "%s" "$COMPONENTS" | sed 's/[[\.*^$()+?{|]/\\&/g')
+    sed -i.bak "s|<components-list>|$COMPONENTS_ESCAPED|g" "$BLUEPRINT_DIR/kustomization.yaml"
+  else
+    # Remove the components-list placeholder if no components are needed
+    sed -i.bak "/<components-list>/d" "$BLUEPRINT_DIR/kustomization.yaml"
+  fi
+  
+  if [[ -n "$LITERALS" ]]; then
+    # Add SERVICE_NAME literal first, then any additional literals
+    FULL_LITERALS="      - SERVICE_NAME=$service_name"
+    if [[ -n "$LITERALS" ]]; then
+      FULL_LITERALS+="\n$LITERALS"
+    fi
+    LITERALS_ESCAPED=$(printf "%s" "$FULL_LITERALS" | sed 's/[[\.*^$()+?{|]/\\&/g')
+    sed -i.bak "s|      - SERVICE_NAME=<service-name>|$LITERALS_ESCAPED|g" "$BLUEPRINT_DIR/kustomization.yaml"
+  else
+    # Just replace the service name placeholder
+    sed -i.bak "s|<service-name>|$service_name|g" "$BLUEPRINT_DIR/kustomization.yaml"
+  fi
+  
+  # Handle image registry placeholder - set a default value
+  sed -i.bak "s|<image-registry>/<project-name>|dev.docker.system.local/elpa-$service_name-tst|g" "$BLUEPRINT_DIR/kustomization.yaml"
+  
+  # Clean up backup file
+  rm -f "$BLUEPRINT_DIR/kustomization.yaml.bak"
+fi
+
+# Clean up any remaining backup files
+find "$BLUEPRINT_DIR" -name "*.bak" -delete
 
 mv "$BLUEPRINT_DIR"/* "$TARGET_DIR"
 rmdir "$BLUEPRINT_DIR"
