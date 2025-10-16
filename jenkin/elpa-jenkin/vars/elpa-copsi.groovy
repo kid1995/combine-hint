fb214e1b1fa1279182d48d4f053f0bdb1cdac2fd
The Kiet Dang <The.Kiet.Dang@signal-iduna.de>
2025-10-16 20:00:00 +0200

    add function

--- jenkin/elpa-jenkin/vars/elpa-copsi.groovy
+++ jenkin/elpa-jenkin/vars/elpa-copsi.groovy
@@ -1,33 +1,46 @@
 import de.signaliduna.BitbucketRepo
+import de.signaliduna.TargetSegment
+import de.signaliduna.CopsiEnvironment
 
 @Field
-String GIT_CLONE_DIR = "cd-repos"
-
+String DEPLOY_PROJECT =  "SDASVCDEPLOY"
 @Field
-String GIT_CREDENTIALS_ID = "jenkins_git_http"
+String DEPLOY_REPO =  "elpa-elpa4"
+@Field
+String JIRA_PROJECT =  "ELPA4"
 
-boolean deployTstFeature()(BitbucketRepo repo, String targetBranch, String serviceName, String serviceSuffix, String jiraTicket, String imageName) {
-    sh "mkdir -p ${GIT_CLONE_DIR}"
-    dir(GIT_CLONE_DIR) {
-        withCredentials([gitUsernamePassword(credentialsId: GIT_CREDENTIALS_ID)]) {
-            def gitUrl = buildGitUrl("https://git.system.local/scm/${repo.projectName}/${repo.repoName}.git")
-            sh "rm -rf ${repo.repoName}"
-            sh "git clone ${gitUrl} --depth 1 -b '${targetBranch}'"
-            dir(repo.repoName) {
-                sh "./clean-feauture.sh ${serviceName} ${jiraTicket}"
-                sh "./deploy-feature.sh ${serviceName} ${serviceSuffix} ${jiraTicket} ${imageName}"
-                sh "git add envs/dev"
-                String changedFiles = sh(script: "git diff --name-only ${targetBranch} origin/${targetBranch}", returnStdout: true).trim()
-                if (changedFiles) {
-                    echo "The following files changed:"
-                    echo "${changedFiles}"
-                    sh 'git push -u origin HEAD'
-                    return true
-                } else {
-                    echo "No changes between the local and remote branches."
-                    return false
+String deployTstFeature(String serviceName, String serviceSuffix, String imageName) {
+    
+    def branchName = branchNameWithoutPrefix()
+    def branchNameStrArray = branchName.split('-')
+    if (branchNameStrArray.size() >= 2 && branchNameStrArray.contains(JIRA_PROJECT)) {
+        def jiraIssue = "${branchNameStrArray[0]}-${branchNameStrArray[1]}"
+        echo "Built Image: ${imageName}"
+
+        si_copsi.createChangeAsPullRequest(
+                new BitbucketRepo(DEPLOY_PROJECT, DEPLOY_REPO),
+                "autodeploy/${branchName}-job-${BUILD_NUMBER}",
+                "${CopsiEnvironment.nop}",
+                [:],
+                {
+                    echo "Führe clean-feature.sh und deploy-feature.sh aus für Feature: ${jiraIssue}"
+                    sh "chmod +x ./clean-feature.sh"
+                    sh "chmod +x ./deploy-feature.sh"
+                    sh "./clean-feature.sh ${serviceName} ${jiraIssue}"
+                    sh "./deploy-feature.sh ${serviceName} ${serviceSuffix} ${jiraIssue} ${imageName}"
+                    sh "git add ./envs/dev"
+                    return "${jiraIssue}: Deploy ${serviceName} mit Image ${imageName}"
                 }
-            }
-        }
-    }
+        )
+    } else {
+            echo "ERROR branchName branch name is invalid: ${branchName} - it should be ELPA4-xxxx-<feature-info>"
+    }       
+}
+
+/** 
+ * @returns branch name without any prefix
+ */
+public String branchNameWithoutPrefix() {    
+    def fullBranchNameArray = si_git.branchName().split("/")
+    return fullBranchNameArray[fullBranchNameArray.size()-1]
 }
